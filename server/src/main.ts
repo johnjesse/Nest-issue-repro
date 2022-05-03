d154263ac039484210f5ec3515021aeb80d67470
John J Wood <johnjessewood@hotmail.com>
2022-05-03 16:45:48 +0100

    feat: enable shutdown hooks

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // TOGGLE ON OFF THIS LINE TO SEE THE ISSUE
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
